ef01e1bad73063b9e5b2637d9477ff55b83c4654
Brandon Bartlett <brandonbartlett@Mac.lan>
2025-06-07 01:41:08 -0500

    reformat: implement state and line item component

---
+++ nutrition-tracker/src/lib/components/NutritionLineItem.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+    let { nutritionItem } = $props();
+</script>
+
+<div class="w-5/6 bg-stone-400 rounded-2xl flex gap-6 px-8 py-5 mx-auto text-lg">
+    <div class="text-black/50 w-1/12 text-center">
+        {nutritionItem.time}
+    </div>
+    <div class=" w-5/12 capitalize">
+        {nutritionItem.amount ? `${nutritionItem.title}: ${nutritionItem.amount}` : nutritionItem.title}
+    </div>
+    <div class=" w-3/12 capitalize">
+        {nutritionItem.calories} calories
+    </div>
+    <div class=" w-3/12 capitalize">
+        {nutritionItem.protein} grams protein
+    </div>
+</div>
--- nutrition-tracker/src/routes/+page.svelte
+++ nutrition-tracker/src/routes/+page.svelte
@@ -1,52 +1,49 @@
 <script lang='ts'>
+    import type { PageProps } from "./$types";
+    import type { NutritionItem } from "$lib/types";
+  import NutritionLineItem from "$lib/components/NutritionLineItem.svelte";
 
-    const items = $state([
-        {
-            title: "yogurt",
-            amount: "2 servings",
-            calories: 180,
-            protein: 10
-        },
-        {
-            title: "breakfast burrito",
-            amount: "1 ",
-            calories: 500,
-            protein: 20
-        },
-        {
-            title: "yogurt",
-            amount: "1 servings",
-            calories: 140,
-            protein: 6
-        },
-        {
-            title: "milk",
-            amount: "36 oz",
-            calories: 675,
-            protein: 36
-        },
-    ])
+    let { data }: PageProps = $props();
 
-    let totals = {calories: 0, protein: 0};
-    items.forEach(item => {
-        totals.calories += item.calories;
-        totals.protein += item.protein;
-        })
+    let nutritionItems: NutritionItem[] = $state(data.items)
 
+    let totalCalories = $derived.by(() => {
+        const caloriesArray = nutritionItems.map(item => item.calories)
+        const totalCals = caloriesArray.reduce(
+            (accumulator, currentValue) => accumulator + currentValue, 0
+        )
+        return totalCals;
+    });
+
+    let totalProtein = $derived.by(() => {
+        const proteinArray = nutritionItems.map(item => item.protein)
+        const totalProtein = proteinArray.reduce(
+            (accumulator, currentValue) => accumulator + currentValue, 0
+        )
+        return totalProtein;
+    });
 
 </script>
 
-<h1>Nutrition Tracker</h1>
-
-    <div>
-        totals: Calories: {totals.calories}   Protein: {totals.protein} grams 
+    <div class="bg-gray-700 text-white p-1 text-right flex gap-8">
+        <div class="ml-6">
+            Totals Calories: {totalCalories}   
+        </div>
+        <div>
+            Total Protein: {totalProtein} grams
+        </div>
+            
+    </div>
+    
+    <div class="w-full flex">
         <button 
-            class="bg-green-700 rounded-2xl px-2"
+            class="rounded-2xl px-2 ml-auto mr-3 text-3xl text-neutral-500 hover:text-black"
             onclick={ () => {
                 console.log('clicked')
-                items.push({            
+                nutritionItems.push({            
                     title: "milk",
                     amount: "36 oz",
+                    time: "8:05am",
                     calories: 675,
                     protein: 36
                 }) 
@@ -55,26 +52,13 @@
         >+</button>
     </div>
 
-
-<table>
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th>Calories</th>
-                <th>Protein</th>
-            </tr>
-            {#each items as item}
-            <tr>
-                <td>
-                    {item.title} -- {item.amount}
-                </td>
-                <td>
-                    {item.calories}
-                </td>
-                <td>
-                    {item.protein} grams
-                </td>
-            </tr>
+{#if (!nutritionItems || nutritionItems.length === 0)}
+    <div class="w-1/2 py-6 mx-auto text-center font-semibold">Add An Item To See It Here</div>
+{:else}
+    <div class="flex flex-col gap-4">
+        {#each nutritionItems as item}
+        <NutritionLineItem nutritionItem={item}/>
         {/each}
-        </thead>
-    </table>
+    </div>
+{/if}
+
