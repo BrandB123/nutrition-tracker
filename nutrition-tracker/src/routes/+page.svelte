b6c5323c25eeee4324db7de3651d3cecfcc72b72
Brandon Bartlett <brandonbartlett@Mac.lan>
2025-06-12 00:43:05 -0500

    feat: add form to add more line items

--- nutrition-tracker/src/routes/+page.svelte
+++ nutrition-tracker/src/routes/+page.svelte
@@ -4,6 +4,8 @@
 
     let { data }: PageProps = $props();
 
+    let showForm = $state(false);
+
     let nutritionItems = $state(data.items || []);
 
     let totalCalories = $derived.by(() => {
@@ -22,6 +24,10 @@
         return totalProtein;
     });
 
+    // TODO: find a way to allow the client to select a time that will then be used in a date object to be submitted to the db
+    let defaultDate = new Date().toLocaleTimeString().replace(' ', ':').split(':').splice(2, 1).join(':')
+    console.log(defaultDate);
+
 </script>
 
     <div class="bg-gray-700 text-white p-1 text-right flex gap-8">
@@ -33,8 +39,8 @@
         </div>
             
     </div>
-    
-    <div class="w-full flex">
+
+    <!-- <div class="w-full flex">
         <button 
             class="rounded-2xl px-2 ml-auto mr-3 text-3xl text-neutral-500 hover:text-black"
             onclick={ () => {
@@ -49,7 +55,50 @@
             }
             }
         >+</button>
+    </div> -->
+
+    <div class="w-full flex">
+        <button 
+            class="rounded-2xl px-2 ml-auto mr-3 text-3xl text-neutral-500 hover:text-black"
+            onclick = { () => showForm = true }
+        >+</button>
     </div>
+
+    {#if showForm }
+    <form action="" class="p-6 w-1/2 mx-auto mb-10 rounded-xl bg-neutral-500 grid grid-cols-3 grid-rows-2 gap-4 relative">
+        <div class="flex flex-col col-span-2">
+            <label for="title">Title</label>
+            <input type="text" name="title" placeholder="Chicken Breast" class="border border-black rounded-md bg-neutral-300 pl-2 py-1">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="time">Time</label>
+            <input type="text" name="time" value={new Date().toLocaleTimeString()} class="border border-black rounded-md bg-neutral-300 pl-2 py-1">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="amount">Amount</label>
+            <input type="text" name="amount" placeholder="8oz, 2 servings, etc." class="border border-black rounded-md bg-neutral-300 pl-2 py-1">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="calories">Calories</label>
+            <input type="number" name="calories" placeholder="250" class="border border-black rounded-md bg-neutral-300 pl-2 py-1">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="protein">Protein (in grams)</label>
+            <input type="number" name="protein" placeholder="30" class="border border-black rounded-md bg-neutral-300 pl-2 py-1">
+        </div>
+        <button type="submit" class="col-start-2 col-end-3 hover:font-semibold">Submit</button>
+        <button 
+            type="button" 
+            class="absolute top-1 right-3 font-light hover:font-semibold"
+            onclick={ () => showForm = false }
+        >x</button>
+    </form>
+{/if}
+    
 
 {#if (!nutritionItems || nutritionItems.length === 0)}
     <div class="w-1/2 py-6 mx-auto text-center font-semibold">Add An Item To See It Here</div>
